057299cf51e6f9e244e34782b981ecf2ec6938f5
Aprendiz <Aprendiz@FONDFPCAAGSD080>
2023-12-21 14:04:15 -0500

    implementacionde  las dependenciasAgregar archivos de proyecto.

---
+++ sistemaVentas.control/Dependencia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using sistemaVentas.datos.dbContext;
+using Microsoft.EntityFrameworkCore;
+using sistemaVentas.datos.Interfaces;
+using sistemaVentas.datos.Implementacion;
+//using sistemaVentas.negocios.Implementacion;
+//using sistemaVentas.negocios.Implementacion;
+
+
+
+namespace sistemaVentas.control
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services,IConfiguration configuration) 
+        {
+            services.AddDbContext<DbContext>(options => {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+
+            services.AddTransient(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            services.AddScoped<IVentaRepository, VentaRepository>();
+
+
+        }
+    }
+}
---
+++ sistemaVentas.datos/Implementacion/VentaRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using sistemaVentas.datos.dbContext;
+using sistemaVentas.datos.Interfaces;
+using sistemaVentas.entity;
+
+namespace sistemaVentas.datos.Implementacion
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbContext;
+
+        public VentaRepository(DbventaContext dbContext): base (dbContext)
+            {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Venta> Registrar(Venta entidad)
+        {
+            Venta VentaGenerada = new Venta();
+
+            using (var transaccion = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach ( DetalleVenta dv in entidad.DetalleVenta)
+                    {
+                        Producto productoEncontrado = _dbContext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+
+                        productoEncontrado.Stock = productoEncontrado.Stock - dv.Cantidad;
+                        _dbContext.Productos.Update(productoEncontrado);
+
+                    }
+                    await _dbContext.SaveChangesAsync();
+
+                    NumeroCorrelativo correlativo = _dbContext.NumeroCorrelativos.Where(n => n.Gestion == "venta").First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaActualizacion = DateTime.Now;
+                    _dbContext.NumeroCorrelativos.Update(correlativo);
+                    await _dbContext.SaveChangesAsync();
+
+                    string ceros = string.Concat(Enumerable.Repeat("0", correlativo.CantidadDigitos.Value));
+                    string numVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    numVenta = numVenta.Substring(numVenta.Length - correlativo.CantidadDigitos.Value, correlativo.CantidadDigitos.Value);
+                    entidad.NumeroVenta = numVenta;
+
+                    await _dbContext.Venta.AddAsync(entidad);
+                    await _dbContext.SaveChangesAsync();
+
+                    VentaGenerada = entidad;
+
+                    transaccion.Commit();
+
+
+
+                } catch (Exception ex)
+                {
+                    transaccion.Rollback();
+                    throw ex;
+
+                }
+            }
+
+            return VentaGenerada;
+        }
+
+        public async Task<List<DetalleVenta>> Reporte(DateTime FechaInicio, DateTime FechaFin)
+        {
+            List<DetalleVenta> listaResumen = await _dbContext.DetalleVenta
+                .Include(v =>v.IdVentaNavigation)
+                .ThenInclude(u =>u.IdUsuarioNavigation)
+                .Include(v =>v.IdVentaNavigation)
+                .ThenInclude(tdv => tdv.IdTipoDocumentoVentaNavigation)
+                .Where(dv => dv.IdVentaNavigation.FechaRegistro.Value.Date >= FechaInicio.Date &&
+                dv.IdVentaNavigation.FechaRegistro.Value.Date <= FechaFin.Date).ToListAsync();
+
+            return listaResumen;
+        }
+    }
+}
+    
